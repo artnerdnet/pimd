e19dbeec9b4d895011741457566a0bbe7db05dd3
artnerdnet <artnerdnet@gmail.com>
2018-09-02 22:30:52 +0200

    Deploy to github pages

--- demo/gulpfile.js
+++ demo/gulpfile.js
@@ -1,5 +1,6 @@
 const gulp = require('gulp')
 const connect = require('gulp-connect')
+const deploy = require('gh-pages');
 
 gulp.task('connect', function () {
   connect.server()
@@ -12,5 +13,13 @@ gulp.task('build', () =>
     .pipe(bro())
     .pipe(gulp.dest('js_dist'))
 )
+
+/**
+ * Push build to gh-pages
+ */
+gulp.task('deploy', function () {
+  return gulp.src("./dist/**/*")
+    .pipe(deploy())
+});
 
 gulp.watch('js/*.js', ['build'])
